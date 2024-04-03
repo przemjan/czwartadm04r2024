ebd9c738581b55647e35c5e1083b013ef94c9255
przemjan <przemjanisz2@gmail.com>
2024-04-03 11:48:28 +0200

    commit nr3

---
+++ src/struktury_dec/BoolIf.java
@@ -0,0 +1,15 @@
+package struktury_dec;
+
+public class BoolIf {
+    public static void main(String[] args) {
+        boolean sun = true;
+
+
+        if(sun){
+            System.out.println("Jest ładnie");
+        }
+        else{
+            System.out.println("Jest brzydko");
+        }
+    }
+}
